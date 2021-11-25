b639096d18d38509f6f780ca7f49285cfd29d693
Olavo <olavokruel@gmail.com>
2021-11-25 21:38:54 +0100

    fix(github-actions): Fixed typo in the semantic release config file

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: "master",
-  repositoryUrl: "https://github.com/OKruel/github-actions-ci-cd/",
+  repositoryUrl: "https://github.com/OKruel/github-actions-ci-cd",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
