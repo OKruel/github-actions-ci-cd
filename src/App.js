877b311b4a755f8ce4bd5e42bdb772554cf6a74e
Olavo <olavokruel@gmail.com>
2021-11-27 13:15:58 +0100

    feat(release notification): notify slack group
    Automated Release Notification
    
    Notify team slack channel when a new release is out.
    Notification contains link to the release.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to the lab!</p>
+        <div>Sending a slack message with a new release</div>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
