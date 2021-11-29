f9fb05d75aee81b8da442bed25207d71065866b2
Olavo <olavokruel@gmail.com>
2021-11-29 15:28:16 +0100

    ci(core package): testing github core package

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -11692,24 +11692,48 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-// As .gitignore prevents to upload the node_modules folder, the libraries do not exist in the Virtual Machine.
-// Solutions in the actions.yaml file
+// As .gitignore prevents to upload the node_modules folder, the libraries do not exist in the Virtual Machine. Solutions in the actions.yaml file
+//! GETTING TO KNOW THE CORE AND GITHUB PACKAGES FUNCTIONALITIES
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
+//! How to communicate ERRORS in the LOGS of the WORKFLOW
+// To enable a better DEBUG experience a environment variable must be set in the repository.
+// The variable must be called ACTIONS_STEP_DEBUG and its value must be set to true.
+// To be able to create error log messages in the log of the virtual machine, the core package can be used.
 try {
+  // This functions will only run the DEBUG mode is active in the repository.
+  // It can be activated be setting a environment variable in form of a secret.
+  // The variable must be called ACTIONS_STEP_DEBUG and its value must be set to true.
+  //! DEBUG
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
+
+  //! INPUT
   const name = core.getInput("who-to-greet");
+  //! SET SECRET
+  //# The secret will appear as *** in the workflow log
+  core.setSecret(name);
+  console.log(`Hello ${name}. The name does not appear because it is a secret`);
 
-  console.log(`Hello ${name}`);
-
-  // Setting the output variables
+  //! SET OUTPUT
   const currentTime = new Date();
   core.setOutput("time", currentTime.toTimeString());
 
+  //! EXPAND LOG
+  // This creates an arrow to expand the object to see better the logs
+  core.startGroup("Logging github object");
+  // The "/t" maked the output be formatted with tabs
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  //! SET VARIABLE
+  // It is possible to set variable that be used in any SUBSEQUENT STEP in the workflow.
+  core.exportVariable("JSVAR", "JS variable");
 } catch (error) {
   // The workfolw can not detect the error in this JS file even if we thow it
-  // The function below will inform and pass the error to the workflow.
+  // The function below will MAKE THE ACTION FAIL and pass the error to the workflow.
   core.setFailed(error.message);
 }
 
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,20 +1,44 @@
-// As .gitignore prevents to upload the node_modules folder, the libraries do not exist in the Virtual Machine.
-// Solutions in the actions.yaml file
+// As .gitignore prevents to upload the node_modules folder, the libraries do not exist in the Virtual Machine. Solutions in the actions.yaml file
+//! GETTING TO KNOW THE CORE AND GITHUB PACKAGES FUNCTIONALITIES
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+//! How to communicate ERRORS in the LOGS of the WORKFLOW
+// To enable a better DEBUG experience a environment variable must be set in the repository.
+// The variable must be called ACTIONS_STEP_DEBUG and its value must be set to true.
+// To be able to create error log messages in the log of the virtual machine, the core package can be used.
 try {
+  // This functions will only run the DEBUG mode is active in the repository.
+  // It can be activated be setting a environment variable in form of a secret.
+  // The variable must be called ACTIONS_STEP_DEBUG and its value must be set to true.
+  //! DEBUG
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
+
+  //! INPUT
   const name = core.getInput("who-to-greet");
+  //! SET SECRET
+  //# The secret will appear as *** in the workflow log
+  core.setSecret(name);
+  console.log(`Hello ${name}. The name does not appear because it is a secret`);
 
-  console.log(`Hello ${name}`);
-
-  // Setting the output variables
+  //! SET OUTPUT
   const currentTime = new Date();
   core.setOutput("time", currentTime.toTimeString());
 
+  //! EXPAND LOG
+  // This creates an arrow to expand the object to see better the logs
+  core.startGroup("Logging github object");
+  // The "/t" maked the output be formatted with tabs
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  //! SET VARIABLE
+  // It is possible to set variable that be used in any SUBSEQUENT STEP in the workflow.
+  core.exportVariable("JSVAR", "JS variable");
 } catch (error) {
   // The workfolw can not detect the error in this JS file even if we thow it
-  // The function below will inform and pass the error to the workflow.
+  // The function below will MAKE THE ACTION FAIL and pass the error to the workflow.
   core.setFailed(error.message);
 }
